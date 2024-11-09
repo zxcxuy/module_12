2811ecb325a271987e60c05c3f747e5af0e23d14
dima <140681941+zxcxuy@users.noreply.github.com>
2024-11-09 16:47:28 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,407 @@
+// const btnArchitecture = document.querySelectorAll('.nav__list_item_link')[0];
+// const btnArtFashion = document.querySelectorAll('.nav__list_item_link')[1];
+// const btnBiography = document.querySelectorAll('.nav__list_item_link')[2];
+// const btnBusiness = document.querySelectorAll('.nav__list_item_link')[3];
+// const btnCraftsHobbies = document.querySelectorAll('.nav__list_item_link')[4];
+// const btnDrama = document.querySelectorAll('.nav__list_item_link')[5];
+// const btnFiction = document.querySelectorAll('.nav__list_item_link')[6];
+// const btnFoodDrink = document.querySelectorAll('.nav__list_item_link')[7];
+// const btnHealthWellbeing = document.querySelectorAll('.nav__list_item_link')[8];
+// const btnHistoryPolitics = document.querySelectorAll('.nav__list_item_link')[9];
+// const btnHumor = document.querySelectorAll('.nav__list_item_link')[10];
+// const btnPoetry = document.querySelectorAll('.nav__list_item_link')[11];
+// const btnPsychology = document.querySelectorAll('.nav__list_item_link')[12];
+// const btnScience = document.querySelectorAll('.nav__list_item_link')[13];
+// const btnTechnology = document.querySelectorAll('.nav__list_item_link')[14];
+// const btnTravelMaps = document.querySelectorAll('.nav__list_item_link')[15];
+// const itemArchitecture = document.querySelectorAll('.nav__list_item')[0];
+// const itemArtFashion = document.querySelectorAll('.nav__list_item')[1];
+// const itemBiography = document.querySelectorAll('.nav__list_item')[2];
+// const itemBusiness = document.querySelectorAll('.nav__list_item')[3];
+// const itemCraftsHobbies = document.querySelectorAll('.nav__list_item')[4];
+// const itemDrama = document.querySelectorAll('.nav__list_item')[5];
+// const itemFiction = document.querySelectorAll('.nav__list_item')[6];
+// const itemFoodDrink = document.querySelectorAll('.nav__list_item')[7];
+// const itemHealthWellbeing = document.querySelectorAll('.nav__list_item')[8];
+// const itemHistoryPolitics = document.querySelectorAll('.nav__list_item')[9];
+// const itemHumor = document.querySelectorAll('.nav__list_item')[10];
+// const itemPoetry = document.querySelectorAll('.nav__list_item')[11];
+// const itemPsychology = document.querySelectorAll('.nav__list_item')[12];
+// const itemScience = document.querySelectorAll('.nav__list_item')[13];
+// const itemTechnology = document.querySelectorAll('.nav__list_item')[14];
+// const itemTravelMaps = document.querySelectorAll('.nav__list_item')[15];
+// const btns = document.querySelectorAll('.nav__list_item_link');
+// const items = document.querySelectorAll('.nav__list_item');
+// export let cotegoryCounter = 1;
+
+// btnArchitecture.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     if(cotegoryCounter != 1) {
+//         cotegoryCounter = 1;
+//         deleteItems();
+//         itemArchitecture.classList.add('active');
+//     }
+// })
+// btnArtFashion.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     if(cotegoryCounter != 2) {
+//         cotegoryCounter = 2;
+//         deleteItems();
+//         itemArtFashion.classList.add('active');
+//     }
+// })
+// btnBiography.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     if(cotegoryCounter != 3) {
+//         cotegoryCounter = 3;
+//         deleteItems();
+//         itemBiography.classList.add('active');
+//     }
+// })
+// btnBusiness.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     if(cotegoryCounter != 4) {
+//         cotegoryCounter = 4;
+//         deleteItems();
+//         itemBusiness.classList.add('active');
+//     }
+// })
+// btnCraftsHobbies.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     if(cotegoryCounter != 5) {
+//         cotegoryCounter = 5;
+//         deleteItems();
+//         itemCraftsHobbies.classList.add('active');
+//     }
+// })
+// btnDrama.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     if(cotegoryCounter != 6) {
+//         cotegoryCounter = 6;
+//         deleteItems();
+//         itemDrama.classList.add('active');
+//     }
+// })
+// btnFiction.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     if(cotegoryCounter != 7) {
+//         cotegoryCounter = 7;
+//         deleteItems();
+//         itemFiction.classList.add('active');
+//     }
+// })
+// btnFoodDrink.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     if(cotegoryCounter != 8) {
+//         cotegoryCounter = 8;
+//         deleteItems();
+//         itemFoodDrink.classList.add('active');
+//     }
+// })
+// btnHealthWellbeing.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     if(cotegoryCounter != 9) {
+//         cotegoryCounter = 9;
+//         deleteItems();
+//         itemHealthWellbeing.classList.add('active');
+//     }
+// })
+// btnHistoryPolitics.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     if(cotegoryCounter != 10) {
+//         cotegoryCounter = 10;
+//         deleteItems();
+//         itemHistoryPolitics.classList.add('active');
+//     }
+// })
+// btnHumor.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     if(cotegoryCounter != 11) {
+//         cotegoryCounter = 11;
+//         deleteItems();
+//         itemHumor.classList.add('active');
+//     }
+// })
+// btnPoetry.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     if(cotegoryCounter != 12) {
+//         cotegoryCounter = 12;
+//         deleteItems();
+//         itemPoetry.classList.add('active');
+//     }
+// })
+// btnPsychology.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     if(cotegoryCounter != 13) {
+//         cotegoryCounter = 13;
+//         deleteItems();
+//         itemPsychology.classList.add('active');
+//     }
+// })
+// btnScience.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     if(cotegoryCounter != 14) {
+//         cotegoryCounter = 14;
+//         deleteItems();
+//         itemScience.classList.add('active');
+//     }
+// })
+// btnTechnology.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     if(cotegoryCounter != 15) {
+//         cotegoryCounter = 15;
+//         deleteItems();
+//         itemTechnology.classList.add('active');
+//     }
+// })
+// btnTravelMaps.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     if(cotegoryCounter != 16) {
+//         cotegoryCounter = 16;
+//         deleteItems();
+//         itemTravelMaps.classList.add('active');
+//     }
+// })
+// const deleteItems = () => {
+//     for(let i = 0; i < items.length; i++){
+//         items[i].classList.remove('active');
+//     }
+// }
+
+let url = new URL(
+    'https://www.googleapis.com/books/v1/volumes?q="subject:Architecture"&key=AIzaSyDW3dLsWOl7fhnyiGOeLz9QzB2RqE3zhQU&printType=books&startIndex=0&maxResults=6&langRestrict=en'
+);
+const loadBooksBtn = document.querySelector(".catalog__btn_item")
+loadBooksBtn.addEventListener("click", (e) => {
+    e.preventDefault(e);
+    let startIndex = parseInt(url.searchParams.get("startIndex"));
+    if(startIndex === 30){
+        url.searchParams.set('startIndex', startIndex+4);
+        fetchData();
+    }
+    else if(startIndex < 30){
+        url.searchParams.set("startIndex", startIndex+6);
+        fetchData();
+    }
+    console.log(startIndex);
+    console.log(url)
+})
+
+
+// Получаем контейнер для книг
+const catalogBooks = document.querySelector(".catalog_books");
+
+// Функция для создания книги
+const createBook = (bookData) => {
+    // Перебираем все книги в полученных данных
+    bookData.items.forEach((item) => {
+        // Создаем контейнер для книги
+        const bookElement = document.createElement("div");
+        bookElement.classList.add("catalog__book");
+
+        // Создаем контейнер для обложки книги
+        const bookCover = document.createElement("div");
+        bookCover.classList.add("catalog__book_cover");
+
+        // Если есть обложка, добавляем ее в контейнер
+        if (
+            item.volumeInfo &&
+            item.volumeInfo.imageLinks &&
+            item.volumeInfo.imageLinks.thumbnail
+        ) {
+            const bookImage = document.createElement("img");
+            bookImage.classList.add("catalog__book_img");
+            bookImage.setAttribute("alt", "Обложка");
+
+            bookImage.setAttribute("src", item.volumeInfo.imageLinks.thumbnail);
+
+            bookCover.appendChild(bookImage);
+        }
+        // Если обложки нет, добавляем placeholder
+        else {
+            const bookPlaceholder = document.createElement("div");
+            bookPlaceholder.classList.add("catalog__book_placeholder");
+
+            const bookPlaceholderText = document.createElement("p");
+            bookPlaceholderText.classList.add("catalog__book_placeholder_text");
+            bookPlaceholderText.textContent = "Обложка отсутствует";
+
+            bookPlaceholder.appendChild(bookPlaceholderText);
+            bookCover.appendChild(bookPlaceholder);
+        }
+
+        // Создаем контейнер для информации о книге
+        const bookInfo = document.createElement("div");
+        bookInfo.classList.add("catalog__book_block_info");
+
+        // Добавляем авторов книги
+        const bookAuthors = document.createElement("p");
+        bookAuthors.classList.add(
+            "catalog__book_block_info_authors",
+            "параграф"
+        );
+        bookAuthors.textContent = item.volumeInfo.authors.join(", ");
+
+        // Добавляем название книги
+        const bookTitle = document.createElement("h1");
+        bookTitle.classList.add("catalog__book_block_info_title");
+        bookTitle.textContent = item.volumeInfo.title;
+
+        // Создаем контейнер для рейтинга книги
+        const bookRating = document.createElement("div");
+        bookRating.classList.add("catalog__book_block_info_rating");
+
+        // Создаем контейнер для звездочек рейтинга
+        const bookRatingStars = document.createElement("div");
+        bookRatingStars.classList.add("catalog__book_block_info_rating_stars");
+
+        // Получаем средний рейтинг книги
+        const averageRating = item.volumeInfo?.averageRating ?? 0;
+
+        // Добавляем звездочки рейтинга
+        for (let i = 0; i < 5; i++) {
+            const star = document.createElementNS(
+                "http://www.w3.org/2000/svg",
+                "svg"
+            );
+            star.classList.add("catalog__book_block_info_rating_stars_star");
+            star.setAttribute("width", "12");
+            star.setAttribute("height", "11");
+            star.setAttribute("viewBox", "0 0 12 11");
+            star.setAttribute("fill", "none");
+
+            const path = document.createElementNS(
+                "http://www.w3.org/2000/svg",
+                "path"
+            );
+            path.setAttribute(
+                "d",
+                "M6 0L7.80568 3.5147L11.7063 4.1459L8.92165 6.9493L9.52671 10.8541L6 9.072L2.47329 10.8541L3.07835 6.9493L0.293661 4.1459L4.19432 3.5147L6 0Z"
+            );
+            star.appendChild(path);
+
+            bookRatingStars.appendChild(star);
+
+            // Если текущая звездочка меньше среднего рейтинга, добавляем ей цвет
+            if (i < averageRating) {
+                star.classList.add("yellow");
+            }
+        }
+
+        // Получаем количество отзывов о книге
+        const ratingsCount = item.volumeInfo.averageRating ?? 0;
+        // Добавляем количество отзывов
+        const bookRatingReviews = document.createElement("p");
+        bookRatingReviews.classList.add(
+            "catalog__book_block_info_rating_reviews"
+        );
+        bookRatingReviews.textContent = `${ratingsCount} отзыв`;
+
+        // Добавляем описание книги
+        const bookDescription = document.createElement("p");
+        bookDescription.classList.add(
+            "catalog__book_block_info_description",
+            "параграф"
+        );
+
+        // Проверяем, существует ли описание до доступа к его подстроке
+        if (item.volumeInfo.description !== undefined) {
+            // Обрезаем описание до 100 символов и добавляем многоточие
+            bookDescription.textContent =
+                item.volumeInfo.description.substring(0, 100) + "...";
+        } else {
+            // Если описание отсутствует, отображаем соответствующее сообщение
+            bookDescription.textContent = "Описание отсутствует";
+        }
+
+        // Собираем все части книги в один контейнер
+        bookInfo.appendChild(bookAuthors);
+        bookInfo.appendChild(bookTitle);
+        bookRating.appendChild(bookRatingStars);
+        bookRating.appendChild(bookRatingReviews);
+        bookInfo.appendChild(bookRating);
+        bookInfo.appendChild(bookDescription);
+
+        // Добавляем цену и покупку книги
+        const bookPrice = document.createElement("h2");
+        bookPrice.classList.add("catalog__book_block_info_price");
+        if (item.saleInfo.saleability === "NOT_FOR_SALE") {
+            bookPrice.classList.add("not_for_sale");
+            bookPrice.textContent = "...";
+            const notSaleableDiv = document.createElement("div");
+            notSaleableDiv.classList.add("catalog__book_block_info_saleable");
+            notSaleableDiv.textContent = "не для продажи";
+            bookInfo.appendChild(bookPrice);
+            bookInfo.appendChild(notSaleableDiv);
+        } else if (item.saleInfo.saleability === "FREE") {
+            const bookButton = document.createElement("button");
+            bookButton.classList.add("catalog__book_block_info_btn");
+            bookButton.setAttribute("type", "button");
+            bookButton.textContent = "приобрести бесплатно";
+            bookPrice.textContent = "бесплатно";
+            bookInfo.appendChild(bookPrice);
+            bookInfo.appendChild(bookButton);
+        } else if (
+            item.saleInfo &&
+            item.saleInfo.retailPrice &&
+            item.saleInfo.retailPrice.currencyCode
+        ) {
+            if (item.saleInfo.retailPrice.currencyCode === "USD") {
+                bookPrice.textContent = `$${item.saleInfo.retailPrice.amount}`;
+            } else if (item.saleInfo.retailPrice.currencyCode === "RUB") {
+                bookPrice.textContent = `₽${item.saleInfo.retailPrice.amount}`;
+            }
+            const bookButton = document.createElement("button");
+            bookButton.classList.add("catalog__book_block_info_btn");
+            bookButton.setAttribute("type", "button");
+            bookButton.textContent = "купить сейчас";
+            bookInfo.appendChild(bookPrice);
+            bookInfo.appendChild(bookButton);
+        } else {
+            // Обрабатываем случай, когда отсутствуют необходимые свойства
+            console.error(
+                "Отсутствуют обязательные свойства для цены и продажи:",
+                item
+            );
+        }
+
+        // Добавляем книгу в контейнер каталога
+        bookElement.appendChild(bookCover);
+        bookElement.appendChild(bookInfo);
+        catalogBooks.appendChild(bookElement);
+    });
+};
+
+const fetchData = async () => {
+    const subjects = {
+        1: "Architecture",
+        2: "Art",
+        3: "Biography&Autobiography",
+        4: "Business",
+        5: "Crafts&Hobbies",
+        6: "Drama",
+        7: "Fiction",
+        8: "Cooking",
+        9: "Health&Fitness",
+        10: "History",
+        11: "Humor",
+        12: "Poetry",
+        13: "Psychology",
+        14: "Science",
+        15: "Technology",
+        16: "Travel",
+    };
+    // url.searchParams.set('q', `"subject:${subjects[cotegoryCounter]}"`);
+    url.searchParams.set("q", `"subject:${subjects[1]}"`);
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        const bookData = await response.json();
+        createBook(bookData);
+    } catch (error) {
+        console.error("Fetch error:", error);
+    }
+};
+fetchData();
